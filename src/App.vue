50a0a822e233beb621da07c43a8c44028dc7a151
Egorov Dmitriy <d.egorov@citynature.ru>
2021-05-04 15:35:29 +0300

    'test'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      {}
+      <router-view/>
+    </component>
   </div>
 </template>
 
+<script>
+  import EmptyLayout from "./layouts/EmptyLayout";
+  import MainLayout from "./layouts/MainLayout";
+  export default {
+
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || 'empty') + '-layout'
+      }
+    },
+    components: {
+      EmptyLayout,
+      MainLayout
+    }
+  }
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/styles/index.css";
 </style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-main-layout">
+    <Header @click="isOpenSidebar = !isOpenSidebar"/>
+    <Sidebar v-model="isOpenSidebar"/>
+
+    <main class="app-content" :class="{full: !isOpenSidebar}">
+      <div class="app-page">
+        <router-view/>
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+export default {
+  name: "MainLayout",
+  data: () => ({
+    isOpenSidebar: true
+  }),
+  components: {
+    Header, Sidebar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
